9f793f053c1c673eeb1f2c3c25e3cd926a8e20cb
kasirajans <kasiece@hotmail.com>
2018-01-17 19:58:52 -0600

    not needed

--- keys.js
+++
@@ -1,7 +0,0 @@
-    module.exports={
-        azureAD:{
-            clientID:'1fe08bb4-5fd6-4745-adf6-12dc1803a4be',
-            clientSecret:'HUX3A1fuiQhb6tWkt9BWKnsfZvPyRZBPwjTj88jqVA4=',
-            tenentID:'kasiazure.onmicrosoft.com'
-        }
-    };
